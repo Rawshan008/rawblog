65c4e31c68471f7ff62f1b3c4a0f02cd053c398c
Rawshan Ali <pust.love008@gmail.com>
2022-03-28 17:09:47 +0600

    add header and Footer

---
+++ src/components/footer/index.js
@@ -0,0 +1,19 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <footer className="bg-slate-600 py-3">
+      <div className="container flex flex-row items-center justify-between">
+        <div className="text-white">Copyright@lemonhive.com</div>
+        <div className="text-white">
+          <a href="">facebook</a>
+          <a href="">facebook</a>
+          <a href="">facebook</a>
+          <a href="">facebook</a>
+        </div>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,12 +1,61 @@
+import { graphql, Link, useStaticQuery } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import React from "react"
 
 const Header = () => {
+  const header = useStaticQuery(graphql`
+    query Header {
+      sanityHeader {
+        siteName
+        logo {
+          asset {
+            gatsbyImageData(placeholder: BLURRED)
+          }
+        }
+        navigation {
+          navigationTitle
+          navItem {
+            link {
+              title
+              slug {
+                current
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+
+  const { siteName, logo, navigation } = header.sanityHeader
+
+  const logoUrl = getImage(logo?.asset)
+
   return (
-    <header className="container flex flex-row justify-between mx-auto">
-      <div className="logo">
-        <h1>Site Title</h1>
+    <header className="bg-slate-600 py-3">
+      <div className="container flex flex-row justify-between items-center mx-auto">
+        <div className="logo">
+          <Link to="/">
+            {logo ? <GatsbyImage image={logoUrl} alt={siteName} /> : siteName}
+          </Link>
+        </div>
+        <ul className="flex flex-row">
+          {navigation.map(item => {
+            return item?.navItem.map(navItem => {
+              return (
+                <li>
+                  <Link
+                    className="block text-white py-2 px-2 transition-all hover:text-teal-500"
+                    to={navItem?.link?.slug?.current}
+                  >
+                    {navItem?.link?.title}
+                  </Link>
+                </li>
+              )
+            })
+          })}
+        </ul>
       </div>
-      <div className="menu">hello</div>
     </header>
   )
 }
