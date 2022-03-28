2cdf5002874a461918392ce26eb04bdaa7463826
Rawshan Ali <pust.love008@gmail.com>
2022-03-28 18:32:11 +0600

    Added Footer Social

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -1,15 +1,40 @@
+import { graphql, Link, useStaticQuery } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import React from "react"
 
 const Footer = () => {
+  const footer = useStaticQuery(graphql`
+    query Footer {
+      sanityFooter {
+        copywrite
+        footerSocial {
+          title
+          icon {
+            asset {
+              gatsbyImageData(placeholder: BLURRED)
+            }
+          }
+          url
+        }
+      }
+    }
+  `)
+
+  const { copywrite, footerSocial } = footer.sanityFooter
+  console.log(footerSocial)
   return (
     <footer className="bg-slate-600 py-3">
       <div className="container flex flex-row items-center justify-between">
-        <div className="text-white">Copyright@lemonhive.com</div>
+        <div className="text-white">{copywrite}</div>
         <div className="text-white">
-          <a href="">facebook</a>
-          <a href="">facebook</a>
-          <a href="">facebook</a>
-          <a href="">facebook</a>
+          {footerSocial.map(item => {
+            const socialImg = getImage(item?.icon?.asset)
+            return (
+              <Link to={item?.url}>
+                <GatsbyImage image={socialImg} alt={item?.title} />
+              </Link>
+            )
+          })}
         </div>
       </div>
     </footer>
